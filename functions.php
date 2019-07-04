60376913cd8da9344197fd7e9a0a96a9dfb4e3f0
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-07-04 19:55:59 +0200

    Bootstrap and newer jQuery are now added and connected

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 
 function seal_script_enqueue() {
-    // adding my style, scripts
+    // adding my css styles
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/seal.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.3.1', 'all');
+    // adding my js scripts
     wp_enqueue_script('customjs', get_template_directory_uri() . '/js/seal.js', array(), '1.0.0', true);
+    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.3.1', true);
 }
 add_action( 'wp_enqueue_scripts', 'seal_script_enqueue');
 
@@ -19,3 +22,10 @@ add_action('init', 'seal_theme_setup');
 
 // Add thumbnails images to posts
 add_theme_support('post_thumbnails');
+
+// delete default wp jQuery and include custom jQuery
+function shapeSpace_include_custom_jquery() {
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.4.1.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');
