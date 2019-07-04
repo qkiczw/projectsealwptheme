c32a0dcb9f5f6475a993ec2ce1632632d586e6a9
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-07-04 20:03:17 +0200

    Little celaning in css and functions.php

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 
 function seal_script_enqueue() {
     // adding my css styles
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/seal.css', array(), '1.0.0', 'all');
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/seal.css', array(), '1.0.0', 'all');
     // adding my js scripts
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.3.1', true);
     wp_enqueue_script('customjs', get_template_directory_uri() . '/js/seal.js', array(), '1.0.0', true);
-    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.3.1', true);
 }
 add_action( 'wp_enqueue_scripts', 'seal_script_enqueue');
 
