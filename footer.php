b69b9f28196e4a51a54cff570cc65fe59c3fb6b0
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-07-07 19:05:18 +0200

    I added bootstrap structure to footer section and picture in col 01

--- footer.php
+++ footer.php
@@ -1,13 +1,23 @@
-               <div class="col-sm-12">
-                    <footer>
-                        <p>This is Footer!!!</p>
-                        <?php wp_nav_menu(array('theme_location_'=>'footer')); ?>
-                    </footer>
-               </div>
-                <?php wp_footer(); ?>
-
-            </div>  <!-- .row -->
-        </div> <!-- .container-fluid -->
-        
-    </body>
-</html>
+                </div>  <!-- .row -->
+            </div> <!-- .container-fluid -->
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3">
+                        <img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/img/seal_logo_white.png">
+                        </div>
+                        <div class="col-lg-3"> 
+                            <?php wp_nav_menu(array('theme_location_'=>'footer')); ?>
+                        </div>
+                        <div class="col-lg-3">
+                            col03
+                        </div>
+                        <div class="col-lg-3">
+                            col04
+                        </div>
+                    </div>
+                </div>
+            </footer>
+             <?php wp_footer(); ?>
+         </body>
+     </html>
