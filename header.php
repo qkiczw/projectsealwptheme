fbf940717b6a3bc1a7529fd6eb020ef6f26ff758
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-07-08 20:31:00 +0200

    Script to display post added.

--- header.php
+++ header.php
@@ -19,8 +19,7 @@
         <![endif]-->
         <header>
                 <nav class="navbar navbar-expand-md navbar-light bg-faded">
-                    <a class="navbar-brand" href="#"><img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_min.png">
-</a>
+                    <a class="navbar-brand" href="#"><img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_min.png"></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php get_header(); ?>
-<h1>This is Index!!!</h1>
+
+<?php 
+if( have_posts() ):
+    while( have_posts() ): the_post(); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="post-title">
+                    <h3><?php the_title(); ?></h3>
+                </div>
+                <div class="post-content">
+                    <p><?php the_content();  ?></p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    <?php endwhile;
+endif;
+    
+?>
+
 <?php get_footer(); ?>
