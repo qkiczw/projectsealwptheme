c85ac987bf96d90cf642a7c9d55658f402173db9
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-09-21 19:27:45 +0200

    Now hamburger menu works properly

--- js/seal.js
+++ js/seal.js
@@ -1,8 +1,10 @@
 console.log('JS Works!!!');
 const hamburger = document.querySelector('.hamburger');
+const mainMenu = document.querySelector('.menu');
 
 const handleHamburgerClick = () => {
     hamburger.classList.toggle('hamburger--active');
+    mainMenu.classList.toggle('menu--hidden');
     console.log('Menu is Open');
 }
 
