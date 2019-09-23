d606a7f0c9085dfc4bdba88ccd4d83b891f9e678
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-09-23 19:42:47 +0200

    Commented stuff removed, changes in media queries resolutions setup, polylang custom styles added

--- page-front-page-pl.php
+++ page-front-page-pl.php
@@ -48,7 +48,7 @@
                 ?>
             </nav> -->
             <!-- New menu -->
-            <nav>
+            <nav class="main-menu">
                 <a class="navbar-brand" href="#"><img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_min.png"></a>
                 <?php wp_nav_menu(array('theme_location_'=>'main')); ?>
                 <button class="hamburger">
@@ -56,7 +56,7 @@
                         <span class="hamburger__inner"></span>
                     </span>
                 </button>
-            </nav>
+            </nav> 
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-6 hero-text">
