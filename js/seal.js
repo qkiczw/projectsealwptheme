3c467e11405a8456f2d11c184163fc38e5986d1e
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-09-30 20:21:35 +0200

    I added background triangles animations

--- js/seal.js
+++ js/seal.js
@@ -4,13 +4,45 @@ const mainMenu = document.querySelector('.menu');
 const polyLangBtn = document.querySelector('.pll-parent-menu-item');
 const polyLangSubMenu = document.querySelector('.sub-menu');
 
-const handleHamburgerClick = () => {
-    hamburger.classList.toggle('hamburger--active');
-    mainMenu.classList.toggle('menu--hidden');
-    console.log('Menu is Open');
+// const handleHamburgerClick = () => {
+//     hamburger.classList.toggle('hamburger--active');
+//     mainMenu.classList.toggle('menu--hidden');
+//     console.log('Menu is Open');
+// }
+
+// hamburger.addEventListener('click', handleHamburgerClick);
+// polyLangBtn.addEventListener('click', () => {
+//     polyLangSubMenu.classList.toggle('sub-menu-open');
+// })
+
+// triangle animations //
+const traingleLeft = document.querySelector('.triangle-left-side');
+const traingleRight = document.querySelector('.triangle-right-side');
+const scrollFromTop = window.scrollY;
+console.log('pozycja Y:', scrollFromTop);
+
+
+const traingleLeftAnimation = ()=> {
+    const scrollFromTop = window.scrollY;
+    console.log('pozycja Y:', scrollFromTop);
+    if( scrollFromTop > 400 && scrollFromTop < 2500) {
+
+        traingleLeft.classList.add('traingle-left-visible');
+    }
+    else{
+        traingleLeft.classList.remove('traingle-left-visible');
+    }
 }
 
-hamburger.addEventListener('click', handleHamburgerClick);
-polyLangBtn.addEventListener('click', () => {
-    polyLangSubMenu.classList.toggle('sub-menu-open');
-})
+const traingleRightAnimation = ()=> {
+    const scrollFromTop = window.scrollY;
+    console.log('pozycja Y:', scrollFromTop);
+    if( scrollFromTop > 1200 && scrollFromTop < 3000) {
+        traingleRight.classList.add('traingle-right-visible');
+    }
+    else{
+        traingleRight.classList.remove('traingle-right-visible');
+    }
+}
+window.addEventListener('scroll', traingleLeftAnimation);
+window.addEventListener('scroll', traingleRightAnimation);
