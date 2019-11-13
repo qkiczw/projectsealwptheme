e5b68d1a0aff5e6f429d83a6812365f2450d1756
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-11-13 20:38:29 +0100

    I am rewriting menu to a new one in RWD - work in progress

--- page-front-page-en.php
+++ page-front-page-en.php
@@ -26,37 +26,21 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <header>
-            <nav class="navbar navbar-expand-md navbar-light bg-faded">
-                <a class="navbar-brand" href="#"><img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_min.png"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <?php
-                    wp_nav_menu([
-                    'menu'            => 'main',
-                    'theme_location'  => 'main',
-                    'container'       => 'div',
-                    'container_id'    => 'bs4navbar',
-                    'container_class' => 'collapse navbar-collapse justify-content-end',
-                    'menu_id'         => false,
-                    'menu_class'      => 'navbar-nav',
-                    'depth'           => 2,
-                    'fallback_cb'     => 'bs4navwalker::fallback',
-                    'walker'          => new bs4navwalker()
-                    ]);
-                ?>
-            </nav>
+        <header class="header">
             <!-- New menu -->
-            <!-- <nav class="main-menu">
-                <a class="navbar-brand" href="#"><img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_min.png"></a>
-                <?php wp_nav_menu(array('theme_location_'=>'main')); ?>
-                <button class="hamburger">
-                    <span class="hamburger__box">
-                        <span class="hamburger__inner"></span>
-                    </span>
-                </button>
-            </nav>  -->
+            <nav class="main-menu">
+                <div class="main-menu-brand">
+                    <a class="navbar-brand" href="#"><img class="nav-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo_min.png"></a>
+                    <button class="hamburger">
+                        <span class="hamburger__box">
+                            <span class="hamburger__inner">x</span>
+                        </span>
+                    </button>
+                </div>
+                <div class="main-menu-items">
+                    <?php wp_nav_menu(array('theme_location_'=>'main')); ?>
+                </div>
+            </nav> 
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-6 hero-text">
