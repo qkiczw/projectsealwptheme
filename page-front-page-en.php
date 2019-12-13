8acaa997a335b71c30d57ccb0b4a3a2c335b9e48
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2019-12-13 17:03:08 +0100

    I applied new header code to the rest of subsites

--- page-front-page-en.php
+++ page-front-page-en.php
@@ -97,7 +97,7 @@ if( have_posts() ):
                     <div class="col-md-7">
                         <div class="about-us__short-info">
                             <p>SEAL Project SEAL is international initiative aimed to develop innovative educational tools used for education and activation of seniors. An international guide of education and activation of seniors is going to be created at the project end.</p>
-                            <a href="http://qki.cba.pl/seal/en/o-nas-en/" class=" btn--blue">More about Us</a>
+                            <a href="#" class=" btn--blue">More about Us</a>
                         </div>
                     </div>
                 </div>
